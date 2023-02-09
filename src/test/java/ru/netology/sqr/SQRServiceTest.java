ff37f14a810180f74cc279d24f646908b36d8f03
Roman Ponomarev <ponomarevr1986@gmail.com>
2023-02-09 12:26:58 +0300

    Parameterized Test added

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,21 +1,22 @@
 package ru.netology.sqr;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SQRServiceTest {
 
 
-    @Test
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/sqr.csv")
 
-    public void sqrAmountAtRangeServiceTest() {
+    public void sqrAmountAtRangeServiceTest(int expected, int rangeFrom, int rangeTill) {
         SQRService service = new SQRService();
 
-        int expected = 3;
-
-        int actual = service.sqrAmountAtRangeService(200, 300);
-
+        int actual = service.sqrAmountAtRangeService(rangeFrom, rangeTill);
 
         assertEquals(expected, actual);
 
